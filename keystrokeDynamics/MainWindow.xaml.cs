7d3983d59777935a7fbc035df46c916432b11800
MichałGawędzki <michal5890@gmail.com>
2020-05-28 20:46:24 +0200

    beginning

--- keystrokeDynamics/MainWindow.xaml.cs
+++ keystrokeDynamics/MainWindow.xaml.cs
@@ -12,17 +12,57 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.IO;
 
-namespace keystrokeDynamics
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+namespace keystrokeDynamics {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+
+		Stopwatch stopwatch;
+		List<long> dwells;
+		string textToRewrite = "nosil wilk razy kilka poniesli i wilka";
+
+		public MainWindow() {
+			InitializeComponent();
+			setDefaultValues();
+		}
+
+		
+
+		private void inputBox_KeyDown(object sender, KeyEventArgs e) {
+			stopwatch = new Stopwatch();
+			stopwatch.Start();
+		}
+
+		private void inputBox_KeyUp(object sender, KeyEventArgs e) {
+
+			stopwatch.Stop();
+			dwells.Add(stopwatch.ElapsedMilliseconds);
+			long measuredTime = stopwatch.ElapsedMilliseconds;
+
+			textToRewriteLabel.Content = e.Key + " " + measuredTime + "ms";
+		}
+
+		private void reset_button_Click(object sender, RoutedEventArgs e) {
+			setDefaultValues();
+		}
+
+		private void setDefaultValues() {
+			textToRewriteLabel.Content = textToRewrite;
+			dwells = new List<long>();
+			inputBox.Text = "";
+		}
+
+		private void save_button_Click(object sender, RoutedEventArgs e) {
+			string dwellsAsString = "";
+			foreach(long d in dwells) {
+				dwellsAsString += d + " ";
+			}
+			File.WriteAllText(name_textblock.Text + ".txt", dwellsAsString);
+
+		}
+	}
 }
