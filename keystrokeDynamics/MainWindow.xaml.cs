7cee47b2c76f2ae3380425e146fa138e23b2794b
joannamaria27 <joan.mk7@gmail.com>
2020-05-29 17:54:12 +0200

    add list and ad metric Euclides and metric Manhattan and flight time

--- keystrokeDynamics/MainWindow.xaml.cs
+++ keystrokeDynamics/MainWindow.xaml.cs
@@ -15,54 +15,136 @@ using System.Windows.Shapes;
 using System.Diagnostics;
 using System.IO;
 
-namespace keystrokeDynamics {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window {
+namespace keystrokeDynamics
+{
 
-		Stopwatch stopwatch;
-		List<long> dwells;
-		string textToRewrite = "nosil wilk razy kilka poniesli i wilka";
+    public class Data // nie wiem czy tak robimy czy inaczej; ale zostawiam
+    {
+        //public int DataId { get; private set; }
+        public string Person { get; private set; }
+        public Key Key { get; private set; }
+        public long DawellTime { get; private set; }
+        public long FlightTime { get; private set; }
 
-		public MainWindow() {
-			InitializeComponent();
-			setDefaultValues();
-		}
+        public Data(string _Person, Key _Key, long _DawellTime, long _FlightTime)
+        {
+            //  DataId = _DataId;
+            Person = _Person;
+            Key = _Key;
+            DawellTime = _DawellTime;
+            FlightTime = _FlightTime;
+        }
+    }
 
-		
 
-		private void inputBox_KeyDown(object sender, KeyEventArgs e) {
-			stopwatch = new Stopwatch();
-			stopwatch.Start();
-		}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
 
-		private void inputBox_KeyUp(object sender, KeyEventArgs e) {
+        List<Data> DataList = new List<Data>();
 
-			stopwatch.Stop();
-			dwells.Add(stopwatch.ElapsedMilliseconds);
-			long measuredTime = stopwatch.ElapsedMilliseconds;
+        Stopwatch stopwatchDawell;
+        Stopwatch stopwatchFlight;
 
-			textToRewriteLabel.Content = e.Key + " " + measuredTime + "ms";
-		}
+        //List<long> dwells;
+        string textToRewrite = "nosil wilk razy kilka poniesli i wilka";
 
-		private void reset_button_Click(object sender, RoutedEventArgs e) {
-			setDefaultValues();
-		}
+        #region Metric
+        private double metricEuklides(long[] X, long[] Y)
+        {
+            double sum = 0;
+            for (int i = 0; i < X.Length; i++)
+            {
+                sum += (X[i] - Y[i]) * (X[i] - Y[i]);
+            }
+            double distance = Math.Sqrt(sum);
+            return distance;
+        }
+        private double metricManhattan(long[] X, long[] Y)
+        {
+            double distance = 0;
+            for (int i = 0; i < X.Length; i++)
+            {
+                distance += Math.Abs(X[i] - Y[i]);
+            }
+            return distance;
+        }
+        private double metricCzebyszew(long[] X, long[] Y) //todo
+        {
+            double distance = 0;
 
-		private void setDefaultValues() {
-			textToRewriteLabel.Content = textToRewrite;
-			dwells = new List<long>();
-			inputBox.Text = "";
-		}
+            return distance;
+        }
+        private double metricMahalanobisa(long[] X, long[] Y) //todo
+        {
+            double sum = 0;
 
-		private void save_button_Click(object sender, RoutedEventArgs e) {
-			string dwellsAsString = "";
-			foreach(long d in dwells) {
-				dwellsAsString += d + " ";
-			}
-			File.WriteAllText(name_textblock.Text + ".txt", dwellsAsString);
+            double distance = Math.Sqrt(sum);
 
-		}
-	}
+            return distance;
+        }
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            setDefaultValues();
+        }
+
+        private void inputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            stopwatchDawell = new Stopwatch();
+            stopwatchDawell.Start();
+        }
+        long time = 0;
+
+        private void inputBox_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            stopwatchFlight.Stop();
+            time = stopwatchFlight.ElapsedMilliseconds;
+            stopwatchDawell.Stop();
+            stopwatchFlight = new Stopwatch();
+            stopwatchFlight.Start();
+            // dwells.Add(stopwatch.ElapsedMilliseconds);
+            DataList.Add(new Data(name_textblock.Name, e.Key, stopwatchDawell.ElapsedMilliseconds, time));
+
+            textInfo.Content = e.Key + " " + stopwatchDawell.ElapsedMilliseconds + "ms";
+        }
+
+        private void reset_button_Click(object sender, RoutedEventArgs e)
+        {
+            setDefaultValues();
+        }
+
+        private void setDefaultValues()
+        {
+            textToRewriteLabel.Content = textToRewrite;
+            //dwells = new List<long>();
+            DataList = new List<Data>();
+            inputBox.Text = "";
+            stopwatchFlight = new Stopwatch();
+        }
+
+        private void save_button_Click(object sender, RoutedEventArgs e)
+        {
+            /* string dwellsAsString = "";
+             foreach (long d in dwells)
+             {
+                 dwellsAsString += d + " ";
+             }
+             File.WriteAllText(name_textblock.Text + ".txt", dwellsAsString);*/
+
+            using (TextWriter tw = new StreamWriter(name_textblock.Text + ".txt"))
+            {
+                foreach (Data s in DataList)
+                    tw.WriteLine("KEY: " + s.Key + " DAWELL TIME: " + s.DawellTime + " FLIGHT TIME: " + s.FlightTime);
+            }
+
+
+        }
+    }
 }
