caa1015a1cce81847831acec0e71664a56e30c8a
joannamaria27 <joan.mk7@gmail.com>
2020-06-03 20:50:03 +0200

    update structure

--- keystrokeDynamics/MainWindow.xaml.cs
+++ keystrokeDynamics/MainWindow.xaml.cs
@@ -18,39 +18,38 @@ using System.IO;
 namespace keystrokeDynamics
 {
 
-    public class Data // nie wiem czy tak robimy czy inaczej; ale zostawiam
+    public class Data
     {
-        //public int DataId { get; private set; }
+        //public int DataId { get; private set; }                                                                  
         public string Person { get; private set; }
-        public Key Key { get; private set; }
-        public long DawellTime { get; private set; }
-        public long FlightTime { get; private set; }
 
-        public Data(string _Person, Key _Key, long _DawellTime, long _FlightTime)
+        Dictionary<Key, long> DwellTime;
+        Dictionary<Key, long> FlightTime;
+
+        public Data(string _Person, Dictionary<Key, long> _DwellTime, Dictionary<Key, long> _FlightTime)
         {
-            //  DataId = _DataId;
+            //  DataId = _DataId;                                                            
             Person = _Person;
-            Key = _Key;
-            DawellTime = _DawellTime;
+            DwellTime = _DwellTime;
             FlightTime = _FlightTime;
         }
+
     }
 
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
+    /// <summary>                                                                            
+    /// Interaction logic for MainWindow.xaml                                                
+    /// </summary>                                                                           
+    ///                                                                                      
     public partial class MainWindow : Window
     {
-
         List<Data> DataList = new List<Data>();
 
-        Stopwatch stopwatchDawell;
+        Stopwatch stopwatchDwell;
         Stopwatch stopwatchFlight;
 
         //List<long> dwells;
-        readonly string textToRewrite = "nosil wilk razy kilka poniesli i wilka";
+        readonly string textToRewrite = "nosilwilkrazykilkaponiesliiwilka";
 
         #region Metric
         private double MetricEuklides(long[] X, long[] Y)
@@ -100,25 +99,46 @@ namespace keystrokeDynamics
             setDefaultValues();
         }
 
+        char[,] quantity = { { 'A', '1' }, { 'B', '1' }, { 'C', '1' }, { 'D', '1' }, { 'E', '1' }, { 'F', '1' }, { 'G', '1' }, { 'H', '1' }, { 'I', '1' }, { 'J', '1' },
+            { 'K', '1' }, { 'L', '1' }, { 'M', '1' }, { 'N', '1' }, { 'O', '1' }, { 'P', '1' }, { 'Q', '1' }, { 'R', '1' }, { 'S', '1' }, { 'T', '1' }, { 'U', '1' },
+            { 'W', '1' }, { 'V', '1' }, { 'X', '1' }, { 'Y', '1' }, { 'Z', '1' } };
+
+
         private void inputBox_KeyDown(object sender, KeyEventArgs e)
         {
-            stopwatchDawell = new Stopwatch();
-            stopwatchDawell.Start();
-        }
-        long time = 0;
-
-        private void inputBox_KeyUp(object sender, KeyEventArgs e)
-        {
-
             stopwatchFlight.Stop();
-            time = stopwatchFlight.ElapsedMilliseconds;
-            stopwatchDawell.Stop();
+
+            if (!FTime.TryAdd(e.Key, stopwatchFlight.ElapsedMilliseconds))
+            {
+                KeyConverter kc = new KeyConverter();
+                string keyChar = kc.ConvertToString(e.Key);
+
+                for (int i = 0; i < 26; i++)
+                {
+                    if ((quantity[i, 0]).Equals(keyChar)) //to do 
+                    {
+                        FTime[e.Key] += stopwatchFlight.ElapsedMilliseconds;
+                        FTime[e.Key] /= ++(quantity[i, 1]); //to do
+                    }
+
+                }
+
+            }
+                        
+            stopwatchDwell = new Stopwatch();
+            stopwatchDwell.Start();
             stopwatchFlight = new Stopwatch();
             stopwatchFlight.Start();
-            // dwells.Add(stopwatch.ElapsedMilliseconds);
-            DataList.Add(new Data(name_textblock.Name, e.Key, stopwatchDawell.ElapsedMilliseconds, time));
+        }
 
-            textInfo.Content = e.Key + " " + stopwatchDawell.ElapsedMilliseconds + "ms";
+        Dictionary<Key, long> DTime = new Dictionary<Key, long>();
+        Dictionary<Key, long> FTime = new Dictionary<Key, long>();
+        private void inputBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            stopwatchDwell.Stop();
+            DTime.Add(e.Key, stopwatchDwell.ElapsedMilliseconds);
+ 
+            //textInfo.Content = e.Key + " " + stopwatchDawell.ElapsedMilliseconds + "ms";
         }
 
         private void reset_button_Click(object sender, RoutedEventArgs e)
@@ -129,7 +149,6 @@ namespace keystrokeDynamics
         private void setDefaultValues()
         {
             textToRewriteLabel.Content = textToRewrite;
-            //dwells = new List<long>();
             DataList = new List<Data>();
             inputBox.Text = "";
             stopwatchFlight = new Stopwatch();
@@ -144,27 +163,28 @@ namespace keystrokeDynamics
              }
              File.WriteAllText(name_textblock.Text + ".txt", dwellsAsString);*/
 
-            using TextWriter tw = new StreamWriter(name_textblock.Text + ".txt");
-            foreach (Data s in DataList)
-                tw.WriteLine("KEY: " + s.Key + " DAWELL TIME: " + s.DawellTime + " FLIGHT TIME: " + s.FlightTime);
+            //using TextWriter tw = new StreamWriter(name_textblock.Text + ".txt");
+            //foreach (Data s in DataList)
+            //    tw.WriteLine( s.Person );
 
+            DataList.Add(new Data(name_textblock.Text, DTime, FTime));
+            using TextWriter tw = new StreamWriter(name_textblock.Text + ".txt");
+
+            foreach (KeyValuePair<Key, long> x in DTime)
+            {
+                tw.WriteLine("Key = {0}, Value Dwell = {1}", x.Key, x.Value);
+
+            }
+            foreach (KeyValuePair<Key, long> y in FTime)
+            {
+                tw.WriteLine("Key = {0}, Value Flight  = {1}", y.Key, y.Value);
+            }
 
         }
 
         public void KNN()
         {
-            long[,] tableValueDawell = new long[textToRewrite.Length, textToRewrite.Length];
-            //todo - okreslanie k 
-            int k = 3;
-            int i = 0;
-            foreach (Data s in DataList)
-            {
-                tableValueDawell[i++,0] = s.DawellTime;
-            }
-
-
-
-
+           
         }
 
 
