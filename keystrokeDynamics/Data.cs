72e5edcf230b207d0423a0de517fa1b93b37eced
MichałGawędzki <michal5890@gmail.com>
2020-06-04 17:45:43 +0200

    metryka manhattan

---
+++ keystrokeDynamics/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace keystrokeDynamics {
+	class Data {
+        //public int DataId { get; private set; }                                                                  
+        public string Person { get; private set; }
+
+        public Dictionary<string, long> dwellTimes { get; private set; }
+        public Dictionary<Key, long> flightTime { get; private set; }
+
+        public Data(string _Person, Dictionary<string, long> _DwellTimes, Dictionary<Key, long> _FlightTime) {
+            //  DataId = _DataId;                                                            
+            Person = _Person;
+            dwellTimes = _DwellTimes;
+            flightTime = _FlightTime;
+        }
+    }
+}
